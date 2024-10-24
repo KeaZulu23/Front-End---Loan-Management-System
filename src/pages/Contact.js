407b98929b7a17920a690f9d0fb049a27c10e376
KeaZulu23 <79447627+KeaZulu23@users.noreply.github.com>
2024-10-24 17:15:15 +0200

    Update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,9 +11,9 @@ const Contact = () => {
       {/* Contact Information */}
       <div className="contact-info">
         <h2>Our Contact Information</h2>
-        <p><strong>Email:</strong> support@lubanziy.com</p>
-        <p><strong>Phone:</strong> +27 12 345 6789</p>
-        <p><strong>Address:</strong> 123 Main Street, Johannesburg, South Africa</p>
+        <p><strong>Email:</strong> zandile@lubanziy.co.za | info@lubanziy.co.za</p>
+        <p><strong>Phone:</strong> +27 65 931 9530</p>
+        <p><strong>Address:</strong> 27 Olienhotkoppie, Arboretum, Richards Bay, Kwa-Zulu Natal, 3900 </p>
       </div>
 
       {/* Contact Form */}
@@ -38,7 +38,7 @@ const Contact = () => {
         <h2>Our Location</h2>
         <iframe
           title="Location Map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4241.600968585192!2d28.044045076056634!3d-26.205629977075635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e950e997254b76f%3A0xcb23235a28364c91!2s123%20Main%20St%2C%20Marshalltown%2C%20Johannesburg%2C%202107!5e1!3m2!1sen!2sza!4v1729765993749!5m2!1sen!2sza"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4144.0874679476265!2d32.06435777613251!3d-28.766500375591818!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1efa237160e37559%3A0x51e22f89ea31fed8!2s27%20Olienhoutkoppie%2C%20Arboretum%2C%20Richards%20Bay%2C%203900!5e1!3m2!1sen!2sza!4v1729782819181!5m2!1sen!2sza"
           width="100%"
           height="400"
           style={{ border: 0 }}
